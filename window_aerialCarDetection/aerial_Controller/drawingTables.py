b5b9c26d8e598fae1d447db55d7855049ffc1ee9
Eufalo <adrian.gallegosanchez@gmail.com>
2019-07-09 23:47:35 +0200

    Add files via upload

--- window_aerialCarDetection/aerial_Controller/drawingTables.py
+++ window_aerialCarDetection/aerial_Controller/drawingTables.py
@@ -1,41 +1,117 @@
 from PyQt5.QtWidgets import QTableWidgetItem
 from PyQt5 import QtCore
-class drawingTables():
-#Método constructor de la clase
-    def car_table(self):
-        #take the detection info in Json
-        info_detection=self.detector.info_trackableObjectsToJson()
-        #create the header
-        header = ["ObjID","DetectionLineas"]
-        #load the header in the table
-        self.tableCarDetect.setColumnCount(2)
-        self.tableCarDetect.setHorizontalHeaderLabels(header)
-        #self.tableCarDetect.setRowCount(5)
-        r=0
-        for object_info in info_detection:
-          c=0
-          for e in range(2):
-              if c==0:
-                  item=QTableWidgetItem(object_info["objectID"])
-                  print(item)
-              else:
-                  if c==1:
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog,QMessageBox,QTableWidgetItem,QWidget,QHeaderView,QLabel
+from PyQt5 import uic
+from PyQt5 import QtCore
+
+def drawing_car_table(self):
+    #take the detection info in Json
+    info_detection=self.detector.info_trackableObjectsToJson()
+    #create the header
+    header = ["ObjID","Linea, direccion"]
+    #load the header in the table
+    self.tableCarDetect.setColumnCount(2)
+    self.tableCarDetect.setHorizontalHeaderLabels(header)
+    self.tableCarDetect.setRowCount(len(info_detection))
+    r=0
+    #decrease loop over the info detection
+    #we can see the last detection in the frist positions
+    for aux_i in range(len(info_detection)-1,-1,-1):
+        c=0
+        for e in range(2):
+            if c==0:
+                item=QTableWidgetItem(str(info_detection[aux_i]['objectID']))
+            elif c==1:
+                #string to contain the line counting
+                lineV_H=""
+                if len(info_detection[aux_i]['linecounted'])>0:
+                    #decrease loop over the line counted
+                    #we can see the last line counted in the left positions
+                    for i in range(len(info_detection[aux_i]['list_flag_VorH'])-1,-1,-1):
+                        #detect vertical line
+                        if info_detection[aux_i]['list_flag_VorH'][i]:
+                            lineV_H+="("+str(info_detection[aux_i]['linecounted'][i]) + "V, "+str(info_detection[aux_i]['direction'][i]) +") "
+                        else:
+                            lineV_H+="("+str(info_detection[aux_i]['linecounted'][i]) + "H, "+ str(info_detection[aux_i]['direction'][i]) +") "
+                item=QTableWidgetItem(str(lineV_H))
+            item.setFlags( QtCore.Qt.ItemIsSelectable |  QtCore.Qt.ItemIsEnabled )
+            self.tableCarDetect.setItem(r,c,item )
+            c=c+1
+        r=r+1
+    #self.table_Categorias.cellClicked.connect(self.cellClick)
+    head = self.tableCarDetect.horizontalHeader()
+    head.setSectionResizeMode(QHeaderView.Stretch)
+    head.setStretchLastSection(True)
+    self.tableCarDetect.repaint()
+def drawing_lines_tableH(self):
+    #take the detection info in Json
+    info_lines_detection=self.detector.info_lines_trackableObjectsToJson()
+    #create the header
+    header = ["LineID","CarID, direccion"]
+    #load the header in the table horizontal
+    self.tableHline.setColumnCount(2)
+    self.tableHline.setHorizontalHeaderLabels(header)
+    self.tableHline.setRowCount(len(info_lines_detection))
+    r=0
+    #decrease loop over the info line detection
+    #we can see the last line in the frist positions
+    for aux_i in range(len(info_lines_detection)-1,-1,-1):
+        if not info_lines_detection[aux_i]['flag_VorH']:
+            c=0
+            for e in range(2):
+                if c==0:
+                    item=QTableWidgetItem(str(info_lines_detection[aux_i]['lineID']))
+                elif c==1:
                     #string to contain the line counting
-                    lineV_H=""
-                    if len(object_info["linecounted"])>0:
-                        for line, flagV_H in object_info["linecounted"],object_info["list_flag_VorH"]:
-                            #detect vertical line
-                            if flagV_H:
-                                lineV_H+="("+line + "V) "
-                            else:
-                                lineV_H+="("+line + "H) "
-                    item=QTableWidgetItem(lineV_H)
-              item.setFlags( QtCore.Qt.ItemIsSelectable |  QtCore.Qt.ItemIsEnabled )
-              self.tableCarDetect.setItem(r,c,item )
-              c=c+1
-          r=r+1
-          #self.table_Categorias.cellClicked.connect(self.cellClick)
-        	head = self.tableCarDetect.horizontalHeader()
-        	head.setSectionResizeMode(QHeaderView.Stretch)
-        	head.setStretchLastSection(True)
-        	self.tableCarDetect.repaint()
+                    lineObjId_Dir=""
+                    #decrease loop over the line counted
+                    #we can see the last line counted in the left positions
+                    for i in range(len(info_lines_detection[aux_i]['objectID'])-1,-1,-1):
+                        lineObjId_Dir+="("+str(info_lines_detection[aux_i]['objectID'][i]) +str(info_lines_detection[aux_i]['direction'][i]) +") "
+
+                    item=QTableWidgetItem(str(lineObjId_Dir))
+                item.setFlags( QtCore.Qt.ItemIsSelectable |  QtCore.Qt.ItemIsEnabled )
+                self.tableHline.setItem(r,c,item )
+                c=c+1
+            r=r+1
+    #self.table_Categorias.cellClicked.connect(self.cellClick)
+    head = self.tableHline.horizontalHeader()
+    head.setSectionResizeMode(QHeaderView.Stretch)
+    head.setStretchLastSection(True)
+    self.tableHline.repaint()
+def drawing_lines_tableV(self):
+    #take the detection info in Json
+    info_lines_detection=self.detector.info_lines_trackableObjectsToJson()
+    #create the header
+    header = ["LineID","CarID, direccion"]
+    #load the header in the table horizontal
+    self.tableVline.setColumnCount(2)
+    self.tableVline.setHorizontalHeaderLabels(header)
+    self.tableVline.setRowCount(len(info_lines_detection))
+    r=0
+    #decrease loop over the info line detection
+    #we can see the last line in the frist positions
+    for aux_i in range(len(info_lines_detection)-1,-1,-1):
+        if info_lines_detection[aux_i]['flag_VorH']:
+            c=0
+            for e in range(2):
+                if c==0:
+                    item=QTableWidgetItem(str(info_lines_detection[aux_i]['lineID']))
+                elif c==1:
+                    #string to contain the line counting
+                    lineObjId_Dir=""
+                    #decrease loop over the line counted
+                    #we can see the last line counted in the left positions
+                    for i in range(len(info_lines_detection[aux_i]['objectID'])-1,-1,-1):
+                        lineObjId_Dir+="("+str(info_lines_detection[aux_i]['objectID'][i]) +str(info_lines_detection[aux_i]['direction'][i]) +") "
+
+                    item=QTableWidgetItem(str(lineObjId_Dir))
+                item.setFlags( QtCore.Qt.ItemIsSelectable |  QtCore.Qt.ItemIsEnabled )
+                self.tableVline.setItem(r,c,item )
+                c=c+1
+            r=r+1
+    #self.table_Categorias.cellClicked.connect(self.cellClick)
+    head = self.tableVline.horizontalHeader()
+    head.setSectionResizeMode(QHeaderView.Stretch)
+    head.setStretchLastSection(True)
+    self.tableVline.repaint()
